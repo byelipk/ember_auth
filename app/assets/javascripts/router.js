b99a5a7349c7cb6d086bacc5ef5fb91fc383ebf1
byelipk <byelipk@gmail.com>
2014-05-07 14:56:10 -0400

    add bootstrap template

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -2,4 +2,13 @@
 
 App.Router.map(function() {
   // this.resource('posts');
+  this.resource('users', function() {
+    this.route('new');
+  });
+
+  this.resource('sessions', function() {
+    this.route('new');
+  });
+
+  this.route('top_secret');
 });
